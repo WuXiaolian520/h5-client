a9a9029a385df6ef82a47ca74effae7627c23a15
吴小莲 <695754968@qq.com>
2024-03-05 09:38:16 +0800

    fix:删除注释

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import { Toast } from 'vant'
-// import 'vant/es/toast/style';
 
 const app = createApp(App)
 app.use(router)
-// app.use(Toast);
+
 app.mount('#app')
